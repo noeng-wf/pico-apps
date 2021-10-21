19abb2939926888ad5db40220700da618a883622
Willi Fluehmann <willi.fluehmann@noser.com>
2021-10-21 18:31:46 +0200

    Add second stage boot loader

--- minimal-blink-rust-bare/src/main.rs
+++ minimal-blink-rust-bare/src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+// Second stage boot loader
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
 #[panic_handler]
 fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
     loop {}
