f88b5138242a085192bd1769e6d70b9a00bb6b98
Willi Fluehmann <willi.fluehmann@noser.com>
2021-10-22 17:57:32 +0200

    Port bare blink application to PAC based application

---
+++ minimal-blink-rust-pac/src/blink.rs
@@ -0,0 +1,44 @@
+use rp2040_pac::Peripherals;
+
+const PICO_DEFAULT_LED_PIN: usize = 25;
+
+fn gpio_init_as_sio_output(p: &mut Peripherals, gpio: usize) {
+    // Set input enable on, output disable off
+    p.PADS_BANK0.gpio[gpio].modify(|_r, w| w.ie().set_bit().od().clear_bit());
+
+    // Zero all fields apart from fsel; we want this IO to do what the peripheral tells it.
+    // This doesn't affect e.g. pullup/pulldown, as these are in pad controls.
+    p.IO_BANK0.gpio[gpio].gpio_ctrl.write(|w| w.funcsel().sio_0());
+
+    // Setup GPIO as output
+    p.SIO.gpio_out_clr.write(|w| unsafe { w.bits(1 << gpio) });
+    p.SIO.gpio_oe_set.write(|w| unsafe { w.bits(1 << gpio) });
+}
+
+fn gpio_put(p: &mut Peripherals, gpio: usize, value: bool) {
+    let mask = 1 << gpio;
+
+    if value {
+        p.SIO.gpio_out_set.write(|w| unsafe { w.bits(mask) });
+    } else {
+        p.SIO.gpio_out_clr.write(|w| unsafe { w.bits(mask) });
+    }
+}
+
+fn sleep_ms(p: &Peripherals, delay_ms: u32) {
+    let base = p.TIMER.timerawl.read().bits();
+    while (p.TIMER.timerawl.read().bits() - base) < (delay_ms * 1000) {}
+}
+
+pub fn blink_loop(p: &mut Peripherals) -> ! {
+    const LED_PIN: usize = PICO_DEFAULT_LED_PIN;
+
+    gpio_init_as_sio_output(p, LED_PIN);
+
+    loop {
+        gpio_put(p, LED_PIN, true);
+        sleep_ms(p, 500);
+        gpio_put(p, LED_PIN, false);
+        sleep_ms(p, 500);
+    }
+}
--- minimal-blink-rust-pac/src/main.rs
+++ minimal-blink-rust-pac/src/main.rs
@@ -1,16 +1,50 @@
 #![no_std]
 #![no_main]
 
+mod blink;
+
 use cortex_m_rt::entry;
+use rp2040_pac::Peripherals;
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
+fn switch_to_xosc(p: &mut Peripherals) {
+    // Enable crystal oscillator
+    p.XOSC.ctrl.modify(|_r, w| w.enable().enable());
+    // Wait until crystal oscillator stable
+    while p.XOSC.status.read().stable().bit_is_clear() {}
+
+    // Switch clk_ref to crystal oscillator (12 MHz)
+    p.CLOCKS.clk_ref_ctrl.modify(|_r, w| w.src().xosc_clksrc());
+    // Divisor = 1
+    p.CLOCKS.clk_ref_div.write(|w| unsafe { w.int().bits(1) });
+
+    // Enable 1 us tick generation (dividing clk_ref by 12)
+    p.WATCHDOG.tick.write(|w| unsafe { w.enable().set_bit().cycles().bits(12) });
+}
+
+fn start_subsystems(p: &mut Peripherals) {
+    // Take used subsystems out of reset state
+    p.RESETS.reset.modify(|_r, w| w.pio0().clear_bit().pads_bank0().clear_bit().io_bank0().clear_bit());
+
+    // Wait until reset state has been left
+    loop {
+        let value = p.RESETS.reset_done.read();
+        if value.pio0().bit_is_set() && value.pads_bank0().bit_is_set() && value.io_bank0().bit_is_set() {
+            break;
+        }
+    }
+}
+
 #[entry]
 fn main() -> ! {
-    let _x = 42;
-    loop {}
+    let mut p = Peripherals::take().unwrap();
+    switch_to_xosc(&mut p);
+    start_subsystems(&mut p);
+
+    blink::blink_loop(&mut p);
 }
 
 #[panic_handler]
