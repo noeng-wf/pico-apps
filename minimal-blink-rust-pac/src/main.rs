f176a34ed5009e2b79462dd39a109fe2b03d2c9a
Willi Fluehmann <willi.fluehmann@noser.com>
2021-10-22 16:23:59 +0200

    Target RP2040 and add PAC crate, empty application

--- minimal-blink-rust-pac/src/main.rs
+++ minimal-blink-rust-pac/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+#[entry]
+fn main() -> ! {
+    let _x = 42;
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
