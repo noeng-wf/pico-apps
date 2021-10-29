a44292c7030cf03f866743ce37e7d135440f265c
Willi Fluehmann <willi.fluehmann@noser.com>
2021-10-29 13:24:13 +0200

    Integrate FreeRTOS build (not yet compiling)

--- minimal-blink-rust-hal-rtos/build.rs
+++ minimal-blink-rust-hal-rtos/build.rs
@@ -28,4 +28,20 @@ fn main() {
     // here, we ensure the build script is only re-run when
     // `memory.x` is changed.
     println!("cargo:rerun-if-changed=memory.x");
+
+    //
+    // FreeRTOS Build
+    //
+    let mut b = freertos_cargo_build::Builder::new();
+
+    // Path to FreeRTOS kernel or set ENV "FREERTOS_SRC" instead
+    b.freertos("FreeRTOS-Kernel");
+    b.freertos_config("src");       // Location of `FreeRTOSConfig.h`
+    b.freertos_port("GCC/ARM_CM0".to_string()); // Port dir relativ to 'FreeRTOS-Kernel/portable'
+    b.heap("heap_4.c".to_string());             // Set the heap_?.c allocator to use from
+                                    // 'FreeRTOS-Kernel/portable/MemMang' (Default: heap_4.c)
+
+    // b.get_cc().file("More.c");   // Optional additional C-Code to be compiled
+
+    b.compile().unwrap_or_else(|e| { panic!("{}", e.to_string()) });
 }
