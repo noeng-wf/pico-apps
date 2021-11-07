4fc82a861c35dde7d7895d35ca4e0c3695e6468b
Willi Fluehmann <willi.fluehmann@noser.com>
2021-11-07 22:42:30 +0100

    Let FreeRTOS do some allocations itself so the application has to provide less functions/variables to FreeRTOS

--- blink-c-rtos/FreeRTOSConfig.h
+++ blink-c-rtos/FreeRTOSConfig.h
@@ -28,11 +28,11 @@
 #define configMESSAGE_BUFFER_LENGTH_TYPE        size_t
 
 /* Memory allocation related definitions. */
-#define configSUPPORT_STATIC_ALLOCATION             1
+#define configSUPPORT_STATIC_ALLOCATION             0
 #define configSUPPORT_DYNAMIC_ALLOCATION            1
 #define configTOTAL_HEAP_SIZE                       10240
-#define configAPPLICATION_ALLOCATED_HEAP            1
-#define configSTACK_ALLOCATION_FROM_SEPARATE_HEAP   1
+#define configAPPLICATION_ALLOCATED_HEAP            0
+#define configSTACK_ALLOCATION_FROM_SEPARATE_HEAP   0
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
