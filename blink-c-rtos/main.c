1c1393be64eba81ab6088a8abeca894948e2f3c0
Willi Fluehmann <willi.fluehmann@noser.com>
2021-11-07 23:00:22 +0100

    Rewrite program to use FreeRTOS

--- blink-c-rtos/main.c
+++ blink-c-rtos/main.c
@@ -6,14 +6,33 @@
 
 #include "pico/stdlib.h"
 
-int main() {
+#include "FreeRTOS.h"
+#include "task.h"
+
+void led_task(void *param) {
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        vTaskDelay(250);
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        vTaskDelay(250);
     }
+}
+
+int main() {
+    TaskHandle_t led_task_handle = NULL;
+    uint32_t status = xTaskCreate(
+        led_task,
+        "led_task",
+        1024,
+        NULL,
+        tskIDLE_PRIORITY,
+        &led_task_handle);
+
+    vTaskStartScheduler();
+
+    // Should never be reached
+    while (true) {}
 }
