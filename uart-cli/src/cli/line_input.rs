b03b595924ce03888991aae8b4cb9e8cb842ccb2
Willi Fluehmann <willi.fluehmann@noser.com>
2021-11-14 12:58:17 +0100

    Example CLI command

--- uart-cli/src/cli/line_input.rs
+++ uart-cli/src/cli/line_input.rs
@@ -1,7 +1,13 @@
+//! Converts character input from a serial line into lines of type 'str'.
+
 pub enum LineInputResult<'a> {
+    /// Character not accepted. Not echo shall be done.
     None,
+    /// Caller should echo the given character (usually the same as entered).
     Echo(u8),
-    Complete(&'a [u8]),
+    /// Caller receives a complete line.
+    /// Note: The caller is responsible to echo a new line.
+    Complete(&'a str),
 }
 
 pub struct LineInput<const MAX_LEN: usize> {
@@ -17,6 +23,7 @@ impl<'a, const MAX_LEN: usize> LineInput<MAX_LEN> {
         }
     }
 
+    /// Notifies about an entered char.
     pub fn feed(&'a mut self, c: u8) -> LineInputResult<'a> {
         match c {
             // Backspace
@@ -30,15 +37,14 @@ impl<'a, const MAX_LEN: usize> LineInput<MAX_LEN> {
             }
             // Return
             b'\r' => {
-                if self.len > 0 {
-                    let line = &self.data[0..self.len];
-                    self.len = 0;
-                    LineInputResult::Complete(line)
-                } else {
-                    LineInputResult::None
-                }
+                // Note: It is also allowed to enter empty lines.
+
+                // Conversion to UTF8 string should not fail because only ASCII chars have been accepted.
+                let line = core::str::from_utf8(&self.data[0..self.len]).unwrap();
+                self.len = 0;
+                LineInputResult::Complete(line)
             }
-            // Printable character
+            // Printable ASCII character
             0x20..=0x7E => {
                 if self.len < self.data.len() {
                     self.data[self.len] = c;
--- uart-cli/src/cli/mod.rs
+++ uart-cli/src/cli/mod.rs
@@ -17,6 +17,8 @@ pub trait Timer {
 pub fn run<T: HalRead<u8> + HalWrite<u8> + Write>(uart: &mut T, _timer: &impl Timer) -> ! {
     let mut input = LineInput::<100>::new();
 
+    print_prompt(uart);
+
     loop {
         match uart.read() {
             Result::Ok(c) => {
@@ -28,8 +30,9 @@ pub fn run<T: HalRead<u8> + HalWrite<u8> + Write>(uart: &mut T, _timer: &impl Ti
                         block!(uart.write(c)).unwrap_or(());
                     }
                     LineInputResult::Complete(line) => {
-                        write!(uart, "\r\n").unwrap();
-                        process_line(uart, line)
+                        print_newline(uart);
+                        process_line(uart, line);
+                        print_prompt(uart);
                     }
                 }
             }
@@ -43,7 +46,43 @@ pub fn run<T: HalRead<u8> + HalWrite<u8> + Write>(uart: &mut T, _timer: &impl Ti
     }
 }
 
-fn process_line<T: Write>(uart: &mut T, line: &[u8]) {
-    let line = core::str::from_utf8(line).unwrap();
-    write!(uart, "Got a line: {}\r\n", line).unwrap();
+fn print_prompt<T: Write>(uart: &mut T) {
+    write!(uart, "> ").unwrap();
+}
+
+fn print_newline<T: Write>(uart: &mut T) {
+    write!(uart, "\r\n").unwrap();
+}
+
+fn process_line<T: Write>(uart: &mut T, line: &str) {
+    let line = line.trim();
+    if line.len() == 0 {
+        return;
+    }
+
+    let mut iter = line.split_whitespace();
+    match iter.next().unwrap() {
+        "print" => {
+            loop {
+                if let Some(word) = iter.next() {
+                    write!(uart, "{} ", word).unwrap();
+                } else {
+                    break;
+                }
+            }
+            print_newline(uart);
+        }
+        "help" => print_help(uart),
+        _ => {
+            write!(uart, "Unknown command\r\n").unwrap();
+            print_newline(uart);
+            print_help(uart);
+        }
+    }
+}
+
+fn print_help<T: Write>(uart: &mut T) {
+    write!(uart, "Supported commands:\r\n").unwrap();
+    write!(uart, "  help            Print this help\r\n").unwrap();
+    write!(uart, "  print <words>   Print the given words\r\n").unwrap();
 }
